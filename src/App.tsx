8b9cdbeec607984ebb4366042ee3d3f88b4c8aa9
RamiChaar <Chaarrami22@gmail.com>
2023-09-27 00:19:07 -0700

    added amplify auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import './styling/App.css';
 
-function App() {
+import { Amplify } from 'aws-amplify';
+import type { WithAuthenticatorProps } from '@aws-amplify/ui-react';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
+
+export function App({ signOut, user }: WithAuthenticatorProps) {
   return (
-    <div className="App">
-      <header className="App-header">
-        hi        
-      </header>
-    </div>
+    <>
+      <h1>Hello {user?.username}</h1>
+      <button onClick={signOut}>Sign out</button>
+    </>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
