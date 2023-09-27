541faffb008fd7506c6dcde9d6f9f30b23b90324
RamiChaar <rchaar3@gatech.edu>
2023-09-26 22:07:06 -0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styling/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        hi        
+      </header>
+    </div>
+  );
+}
+
+export default App;
