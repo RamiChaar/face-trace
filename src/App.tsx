cbf2f8c062be2e1c39775abb4600a3132fc661fd
RamiChaar <Chaarrami22@gmail.com>
2023-09-28 20:35:15 -0700

    Added faceapi detection models, set up face detection
    Added models file to public directory. Created a detection function that is set off in an interval to continually pull from the faceApi.

--- src/App.tsx
+++ src/App.tsx
@@ -13,56 +13,114 @@ import type { RootState } from './redux/store'
 import { useSelector, useDispatch } from 'react-redux'
 import { decrement, increment } from './redux/exampleSlice'
 
+//faceApi
+import * as faceApi from 'face-api.js';
+
 Amplify.configure(awsConfig);
 
-export function App({ signOut, user }: WithAuthenticatorProps) {
+type Detection = faceApi.WithAge<faceApi.WithGender<faceApi.WithFaceExpressions<{ detection: faceApi.FaceDetection; }>>>;
+
+export function App({signOut, user} : WithAuthenticatorProps) {
   /*============ redux example code
   const count = useSelector((state: RootState) => state.example.value)
   const dispatch = useDispatch()
   ============*/
 
   const [playing, setPlaying] = useState<boolean>(false)
-
+  const [currentInterval, setCurrentInterval] = useState<NodeJS.Timer>()
 
   const videoRef = useRef<HTMLVideoElement>(null)
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+
+
+  const INTERVAL_TIME : number = 200
+  
+
+  useEffect(()=>{
+    loadModels()
+  },[])
+
+  const loadModels = ()=>{
+    Promise.all([
+      faceApi.nets.tinyFaceDetector.loadFromUri("/models"),
+      faceApi.nets.faceLandmark68Net.loadFromUri("/models"),
+      faceApi.nets.faceRecognitionNet.loadFromUri("/models"),
+      faceApi.nets.faceExpressionNet.loadFromUri("/models"),
+      faceApi.nets.ageGenderNet.loadFromUri("/models"),
+    ]).then(()=>{
+      console.log('models loaded')
+    })
+  }
 
   const startVideo = () => {
-    console.log('click')
-    if (playing) {
-      return
-    }
 
-		navigator.mediaDevices.getUserMedia({
-				video: {width: 1920, height: 1080}
-			})
-      .then(async (stream) => {
+    navigator.mediaDevices.getUserMedia({
+      video: {
+        width: 1280, 
+        height: 720
+      }
+    })
+    .then(async(stream : MediaStream) => {
+      if (!videoRef.current) {return}
+      const video : HTMLVideoElement = videoRef.current
+      video.srcObject = stream
+      await video.play()
+      video.style.zIndex = '0'
+      setCurrentInterval(setInterval(detect, INTERVAL_TIME))
+      setPlaying(true)
+    })
+    .catch((err) => {
+      console.error(err)
+    })
 
-				let video = videoRef.current
-        if(video) {
-          video.srcObject = stream
-          await video.play()
-          setPlaying(true)
-        } 
+  }
 
-			})
-      .catch((err) => {
-        console.error(err)
-      })
-	}
+  const stopVideo = async () => {
 
-	const stopVideo = () => {
-		setPlaying(false)
+    if (!videoRef.current) {return}
 
-    let video = videoRef.current
-    if (!video) {return}
+    const video : HTMLVideoElement= videoRef.current
 
-    let stream : any = video.srcObject
+    let stream : any = video.srcObject 
 
-    if(!stream) {return}
-  
-    stream.getTracks().forEach( function (track : MediaStreamTrack) {
+    await stream.getTracks().forEach( function (track : MediaStreamTrack) {
       track.stop();
     })
+
+    setPlaying(false)
+    clearInterval(currentInterval)
+    video.style.zIndex = '2'
+
+  }
+
+  const detect = async () => {
+    if (!videoRef.current || !canvasRef.current) {return}
+
+    const video : HTMLVideoElement = videoRef.current
+    const canvas : HTMLCanvasElement = canvasRef.current
+
+    const detections : Detection[] = await faceApi.detectAllFaces(video, new faceApi.TinyFaceDetectorOptions())
+    .withFaceExpressions()
+    .withAgeAndGender()
+
+    const dimensions = {width:1280,height:720}
+    faceApi.matchDimensions(canvas, dimensions)
+    const resizedDetections : Detection[] = faceApi.resizeResults(detections, dimensions)
+
+    for(let i : number = 0; i < detections.length; i++)  {
+
+      const text : string[] = [
+        `${detections[i].age.toFixed(1)} years`,
+        `${detections[i].gender} (${detections[i].genderProbability.toFixed(2)})`
+      ]
+
+      const anchor : faceApi.IPoint = { x: detections[i].detection.box.x, y: detections[i].detection.box.y  + detections[i].detection.box.height}
+      const drawBox : faceApi.draw.DrawTextField = new faceApi.draw.DrawTextField(text, anchor)
+      drawBox.draw(canvas)
+
+    }
+
+    faceApi.draw.drawDetections(canvas,resizedDetections)
 
   }
 
@@ -73,12 +131,13 @@ export function App({ signOut, user }: WithAuthenticatorProps) {
       <button onClick={signOut}>Sign out</button>
 
       <div className='videoStreamDiv'>
-        <video ref={videoRef} className='videoStream'></video>
+        
+        <video ref={videoRef}  className='videoStream'></video>
+        <canvas ref={canvasRef} width="720" height="560" className="canvasOverlay"/>
 
         {playing ? (<button onClick={stopVideo} className='toggleStreamButton'>End</button>) : (<button onClick={startVideo} className='toggleStreamButton'>Start</button>)}
 
       </div>
-
 
       {/* Redux Example JSX
       <div>
